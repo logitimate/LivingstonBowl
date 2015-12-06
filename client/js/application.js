d1a8da42be4f00b89092337b2801dc7ef63e3f95
Logan Livingston <loganlivingston@Logans-Air.attlocal.net>
2015-12-05 22:33:14 -0600

    messing with Google Analytics

--- client/js/application.js
+++ client/js/application.js
@@ -15,3 +15,5 @@ Meteor.myFunctions = {
             });
         }
     }
+
+    
--- client/js/routes.js
+++ client/js/routes.js
@@ -1,17 +1,21 @@
 if (Meteor.isClient) {
     Router.route('/', function() {
+        trackPageView: true
         this.render('home');
     });
 
     Router.route('/scoreboard', function() {
+         trackPageView: true
         this.render('scoreboard');
     });
 
     Router.route('/bowlPicks', function() {
+         trackPageView: true
         this.render('bowlPicks');
     });
 
     Router.route('/admin', function() {
+         trackPageView: true
         this.render('admin');
     });
 }
