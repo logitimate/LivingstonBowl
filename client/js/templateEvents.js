c2a387bcebefedad77db16637a43865c8408692d
Logan Livingston <loganlivingston@Logans-Air.attlocal.net>
2015-12-05 19:21:54 -0600

    [logan] doing check to remove submit button if first bowl game has already happened

--- client/js/templateEvents.js
+++ client/js/templateEvents.js
@@ -124,7 +124,19 @@ if (Meteor.isClient) {
 
     Template.bowlPicks.helpers({
         bowls : function(){
-            return Bowls.find().fetch();
+            return Bowls.find({},{sort:{'date':1}}).fetch();
+        },
+        isNotLocked: function(){
+            var firstBowl = Bowls.find({}, {sort: {date:1}, limit:1}).fetch();
+            var firstBowlDate = firstBowl[0].date;
+            var today = moment(new Date());
+            return moment(firstBowlDate).isAfter(today);
+        },
+        isLocked: function(){
+            var firstBowl = Bowls.find({}, {sort: {date:1}, limit:1}).fetch();
+            var firstBowlDate = firstBowl[0].date;
+            var today = moment(new Date());
+            return !moment(firstBowlDate).isAfter(today);
         },
         isCorrect: function(params) {
             var pick = Picks.findOne({'name': params.hash.bowlName, 'season': Number(params.hash.season), 'owner': Meteor.userId()});
