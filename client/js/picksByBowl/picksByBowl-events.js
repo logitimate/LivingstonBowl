73ff959e7b14a908691041506d6d02801b3bb53d
Logan Livingston <loganlivingston@Logans-Air.attlocal.net>
2015-12-06 19:17:42 -0600

    [logan] added names to modal on all scoreboard

--- client/js/picksByBowl/picksByBowl-events.js
+++ client/js/picksByBowl/picksByBowl-events.js
@@ -1,3 +1,12 @@
 if (Meteor.isClient) {
-	Template.bowlPicks.events({})
+	Template.picksByBowl.events({
+		'click .pick' : function(event){
+			console.log(event.currentTarget);
+			var team = $(event.currentTarget).data('team');
+			var picks = $(event.currentTarget).data('picks');
+			Session.set('team', team );
+			Session.set('picks',picks);
+			$('#teamPicksModal').openModal();
+		}
+	})
 }
--- client/js/picksByBowl/picksByBowl-helpers.js
+++ client/js/picksByBowl/picksByBowl-helpers.js
@@ -1,28 +1,51 @@
 if (Meteor.isClient) {
     Template.picksByBowl.rendered = function() {
-         $('.modal-trigger').leanModal();
-     };
+        $('.modal-trigger').leanModal();
+    };
 
     Template.picksByBowl.helpers({
         bowls: function() {
-            return _.sortBy(Bowls.find({}).fetch(), function(bowl){
+            return _.sortBy(Bowls.find({}).fetch(), function(bowl) {
                 return new Date(bowl.date);
             });
         },
-        picks : function(params){
-            var picks = Picks.find({'season':Number(params.hash.season), 'name': params.hash.name, 'choice':params.hash.team}).fetch();
-            return JSON.stringify(_.map(picks, function(pick){
-                pick['ownerName'] = Meteor.users.findOne({'_id':pick.owner}).name;
-                return pick;
+        picks: function(params) {
+            var picks = Picks.find({
+                'season': Number(params.hash.season),
+                'name': params.hash.name,
+                'choice': params.hash.team
+            }).fetch();
+            return JSON.stringify(_.map(picks, function(pick) {
+                return Meteor.users.findOne({
+                    '_id': pick.owner
+                }).profile.name;
             }));
         },
-        counts : function(params){
-            var picks = Picks.find({'season':Number(params.hash.season), 'name': params.hash.name, 'choice':params.hash.team}).fetch();
+        counts: function(params) {
+            var picks = Picks.find({
+                'season': Number(params.hash.season),
+                'name': params.hash.name,
+                'choice': params.hash.team
+            }).fetch();
             return picks.length;
         },
-        username: function(params){
-            var name = Meteor.users.find({_id: params.hash.owner}).fetch()[0].profile.name
+        username: function(params) {
+            var name = Meteor.users.find({
+                _id: params.hash.owner
+            }).fetch()[0].profile.name
             return name;
         }
     });
+
+    Template.teamPicksModal.helpers({
+        teamPicks: function() {
+            var picks = Session.get('picks');
+            return picks;
+        },
+
+        team: function(){
+            var team = Session.get('team');
+            return team;
+        }
+    })
 }
